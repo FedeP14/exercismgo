b600e7b2d4c9279a4abe4a6ef1409b6aff3bcad0
Federico Pulcino <f.pulcino@campus.unimib.it>
2022-09-26 21:16:58 +0200

    election date and some changes

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,46 +1,19 @@
 package logs
 
-import (
-	"math"
-	"strings"
-)
-
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	indexReccomendation := strings.IndexRune(log, '❗')
-	indexSearch := strings.IndexRune(log, '🔍')
-	indexWeather := strings.IndexRune(log, '☀')
-
-	if indexReccomendation == -1 && indexSearch == -1 && indexWeather == -1 {
-		return "default"
+	app := map[rune]string{
+		'\u2757':     "recommendation",
+		'\U0001f50d': "search",
+		'\u2600':     "weather",
 	}
 
-	if indexSearch == -1 {
-		indexSearch = len(log)
+	for _, char := range log {
+		if name, ok := app[char]; ok {
+			return name
+		}
 	}
-
-	if indexWeather == -1 {
-		indexWeather = len(log)
-	}
-
-	if indexReccomendation == -1 {
-		indexReccomendation = len(log)
-	}
-
-	min := math.Min(float64(indexReccomendation), float64(indexSearch))
-
-	min = math.Min(min, float64(indexWeather))
-
-	switch {
-	case min == float64(indexReccomendation):
-		return "recommendation"
-	case min == float64(indexWeather):
-		return "weather"
-	case min == float64(indexSearch):
-		return "search"
-	}
-
-	return ""
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
